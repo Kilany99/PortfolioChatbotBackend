2fe4b02d30c72fb305902b6eb9306577baa16701
kilany99 <asdghalb@gmail.com>
2025-05-13 14:45:40 +0300

    FIx: Update CORS to allow requests from Netlify app

--- PortfolioChatbotBackend/Program.cs
+++ PortfolioChatbotBackend/Program.cs
@@ -76,7 +76,12 @@ builder.Services.AddCors(options =>
             policy.WithOrigins("http://localhost:4200")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
+            policy.WithOrigins("https://abdalla-elkilany.netlify.app")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
         });
+
+
 });
 
 var app = builder.Build();
